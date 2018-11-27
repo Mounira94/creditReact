b4f9f5aa275d9030e8682936c53b40c153a63598
Mounira94 <mounira.sghari94@gmail.com>
2018-11-27 15:15:22 +0100

    Updates

---
+++ precache-manifest.82a84b5ecf33778f275e24052e40d9e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "301c4cb50acbe659fc7488f447566773",
+    "url": "/creditReact/static/media/planete.301c4cb5.jpg"
+  },
+  {
+    "revision": "df562a069f82ca155a0d92b3b8e711bf",
+    "url": "/creditReact/static/media/Capture.df562a06.PNG"
+  },
+  {
+    "revision": "6eed91166697b5ea2b73",
+    "url": "/creditReact/static/js/runtime~main.6eed9116.js"
+  },
+  {
+    "revision": "cf3cc16447c19c107dfc",
+    "url": "/creditReact/static/js/main.cf3cc164.chunk.js"
+  },
+  {
+    "revision": "1c8e859c0316e3cabd3e",
+    "url": "/creditReact/static/js/1.1c8e859c.chunk.js"
+  },
+  {
+    "revision": "cf3cc16447c19c107dfc",
+    "url": "/creditReact/static/css/main.59790aaf.chunk.css"
+  },
+  {
+    "revision": "f7ef3c9c85d6245ac2c756cad3cae17a",
+    "url": "/creditReact/index.html"
+  }
+];
